df1d8f8c268cbb617969dc52c124dcef5fff00d5
gabrielclongo <gabrielcinzas@gmail.com>
2026-03-24 15:11:27 +0000

    Clean up Inspection.cs by removing conflict markers
    Removed merge conflict markers from Inspection.cs.

--- FoodInspectionApp/Models/Inspection.cs
+++ FoodInspectionApp/Models/Inspection.cs
@@ -16,10 +16,7 @@ namespace FoodInspectionApp.Models
 
         public string Notes { get; set; } = string.Empty;
 
-<<<<<<< HEAD
-        
-=======
->>>>>>> a9d32beef713fb0aa1166113bdecd89801d7c7ee
+
         public Premises? Premises { get; set; }
 
         public List<FollowUp> FollowUps { get; set; } = new();
